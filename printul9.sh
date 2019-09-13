6210258f55c506956ffa9871a2ed1928f03b945f
emue25 <48172339+emue25@users.noreply.github.com>
2019-09-13 10:30:31 +0800

    Update printul9.sh

--- printul9.sh
+++ printul9.sh
@@ -8,8 +8,16 @@ apt-get install ufw
 apt-get install sudo
 
 # Install Pritunl
-echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list
-echo "deb http://repo.pritunl.com/stable/apt stretch main" > /etc/apt/sources.list.d/pritunl.list
+#echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list
+#echo "deb http://repo.pritunl.com/stable/apt stretch main" > /etc/apt/sources.list.d/pritunl.list
+sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list << EOF
+deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main
+EOF
+
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt stretch main
+EOF
+
 sudo apt-get install dirmngr
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
